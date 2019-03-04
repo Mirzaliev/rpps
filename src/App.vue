8dd7c940573e5ed47bff153219a394820451834b
arsen <89282872668>
2019-03-04 23:46:28 +0300

    create Nav component end create dropdown

--- src/App.vue
+++ src/App.vue
@@ -4,42 +4,7 @@
       <div class="header-logo">
         <a class="header-logo__siteName" href="/">Рейтинг ППС</a>
       </div>
-      <nav class="header-nav">
-        <div class="header-nav-item">
-          <a href="#" class="header-nav-item__link">Длинное название</a>
-        </div>
-        <div class="header-nav-item">
-          <a href="#" class="header-nav-item__link">Топ 100</a>
-        </div>
-        <div class="header-nav-item">
-          <div class="dropdown-wrapper">
-            <a class="dropdown-wrapper__title">
-              <span class="title">Классификаторы</span>
-              <eva-icon name="arrow-down" width="18" height="18" fill="#ccc"></eva-icon>
-            </a>
-            <ul class="nav-dropdown">
-              <li class="nav-dropdown-item"><a href="#" class="nav-dropdown-item__link">Ссылка 1</a></li>
-              <li class="nav-dropdown-item"><a href="#" class="nav-dropdown-item__link">Ссылка 2</a></li>
-              <li class="nav-dropdown-item"><a href="#" class="nav-dropdown-item__link">Ссылка 3 Длинная</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="header-nav-item">
-          <div class="dropdown-wrapper">
-            <a class="dropdown-wrapper__title">
-              <span class="title">Кафедры</span>
-              <eva-icon name="chevron-down" width="18" height="18" fill="#ccc"></eva-icon>
-            </a>
-            <!--<ul>-->
-            <!--<li><a href="#">Ссылка 1</a></li>-->
-            <!--<li><a href="#">Ссылка 2</a></li>-->
-            <!--<li><a href="#">Ссылка 3 Длинная</a></li>-->
-            <!--</ul>-->
-          </div>
-        </div>
-        <!--<router-link to="/" class="header-nav__link">Home</router-link> |-->
-        <!--<router-link to="/about" class="header-nav__link">About</router-link>-->
-      </nav>
+      <Nav></Nav>
       <div class="header-user">
         <a href="" class="button button_radius_40 button_withborder_blue">
           Вход
@@ -50,6 +15,24 @@
   </div>
 </template>
 
+<script>
+import Nav from "@/components/menu/Nav.vue";
+
+export default {
+  name: "App",
+  components: {
+    Nav
+  },
+  data() {
+    return {
+      hover: false
+    };
+  },
+
+};
+</script>
+
+
 <style lang="sass">
 .header
   display: flex
@@ -57,56 +40,13 @@
   padding: 15px 50px
   border-bottom: 1px solid #eaecef
   align-items: center
+  font-size: 16px
   &-logo
     padding: 0 10px
     &__siteName
       white-space: nowrap
       font-weight: 600
       font-size: em(25)
-  &-nav
-    width: 100%
-    display: flex
-    justify-content: center
-    align-items: center
-    color: black
-    &-item
-      position: relative
-      margin-left: em(20)
-      &__link
-        display: block
-        font-size: em(14)
-        line-height: 1.4
-        transition: color ease .5s
-        &:hover
-          color: $linkHover
-      .dropdown-wrapper
-        cursor: pointer
-        font-size: em(14)
-        &__title
-          display: flex
-          align-items: center
-          padding: 5px 0
-          svg
-            margin-top: em(3)
-            margin-left: em(3)
-          &:hover
-            color: #F43F3B
-            .nav-dropdown
-              display: none!important
-        .nav-dropdown
-          height: auto!important
-          box-sizing: border-box
-          max-height: calc(100vh - 2.7rem)
-          overflow-y: auto
-          position: absolute
-          top: 100%
-          right: 0
-          background-color: white
-          padding: .6rem 1em
-          white-space: nowrap
-          margin: 0
-          border-radius: 3px
-          border: 1px solid #ccc
   &-user
     padding: 0 em(10)
 </style>
---
+++ src/components/menu/Nav.vue
@@ -0,0 +1,104 @@
+<template>
+    <nav class="header-nav">
+        <div class="header-nav-item">
+            <a href="#" class="header-nav-item__link">Длинное название</a>
+        </div>
+        <div class="header-nav-item">
+            <a href="#" class="header-nav-item__link">Топ 100</a>
+        </div>
+        <div class="header-nav-item">
+            <div class="dropdown">
+                <a class="header-nav-item__link dropdown_link">
+                    <span class="title">Классификаторы</span>
+                    <eva-icon name="arrow-down" width="18" height="18" fill="#ccc"></eva-icon>
+                </a>
+                <ul class="dropdown-list">
+                    <li class="dropdown-list-item"><a href="#" class="dropdown-list-item__link">Ссылка 1</a></li>
+                    <li class="dropdown-list-item"><a href="#" class="dropdown-list-item__link">Ссылка 2</a></li>
+                    <li class="dropdown-list-item"><a href="#" class="dropdown-list-item__link">Ссылка 3 Длинная</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--<div class="header-nav-item">-->
+            <!--<div class="dropdown-wrapper">-->
+                <!--<a class="dropdown-wrapper__title">-->
+                    <!--<span class="title">Кафедры</span>-->
+                    <!--<eva-icon name="chevron-down" width="18" height="18" fill="#ccc"></eva-icon>-->
+                <!--</a>-->
+                <!--<ul class="nav-dropdown">-->
+                    <!--<li class="nav-dropdown-item"><a href="#" class="nav-dropdown-item__link">Ссылка 1</a></li>-->
+                    <!--<li class="nav-dropdown-item"><a href="#" class="nav-dropdown-item__link">Ссылка 2</a></li>-->
+                    <!--<li class="nav-dropdown-item"><a href="#" class="nav-dropdown-item__link">Ссылка 3 Длинная</a></li>-->
+                <!--</ul>-->
+            <!--</div>-->
+        <!--</div>-->
+    </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
+
+<style lang="sass">
+.header
+  &-nav
+    width: 100%
+    display: flex
+    justify-content: center
+    align-items: center
+    color: black
+    &-item
+      position: relative
+      margin-left: em(20)
+      &:first-child
+        margin-left: 0
+      &__link
+        padding: 10px 0
+        font-size: em(14)
+        font-weight: bold
+        line-height: 1.4
+        transition: color ease .5s
+        &:hover
+          color: $linkHover
+    .dropdown
+      cursor: pointer
+      &_link
+        display: flex
+        align-items: center
+        .eva-hover
+          width: 18px
+          height: 18px
+        &:hover
+          color: black
+      .dropdown-list
+        display: flex
+        flex-direction: column
+        align-items: center
+        justify-content: center
+        box-sizing: border-box
+        position: absolute
+        top: 100%
+        left: 50%
+        transform: translateX(-50%)
+        pointer-events: none
+        z-index: 1000
+        background-color: #fff
+        padding: em(15)
+        border: 1px solid #ccc
+        border-radius: 6px
+        white-space: nowrap
+        margin: 0
+        &-item
+          padding: em(5) 0
+          &__link
+            font-size: em(14)
+            font-weight: bold
+            color: inherit
+            &:hover
+              color: $linkHover
+
+
+
+</style>
